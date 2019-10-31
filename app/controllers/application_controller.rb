cb49e837023c141da1fd20ce8c526d5ea53e8a12
ratsimisampy <ratsimisampy@yahoo.fr>
2019-11-01 01:00:46 +0300

    je peux register/new mais je ne peux pas session/new avec un username...

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
- 
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+  
+    def configure_permitted_parameters
+      added_attrs = [:username, :email, :password, :password_confirmation, :remember_me]
+      devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
+      devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+      devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+    end
+
+    attr_writer :login
+
+    def login
+      @login || self.username || self.email
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable  
+         :recoverable, :rememberable, :validatable, authentication_keys: [:login]  
+  validates :username, presence: true, uniqueness: true, length: {minimum: 3, maximum: 25}, uniqueness: { case_sensitive: false }
 end
