00dcb3577b080b875426b87a3dc62b6835785895
ratsimisampy <ratsimisampy@yahoo.fr>
2019-11-22 15:24:23 +0300

    ajout like/unlike sur articles

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,14 +1,39 @@
 class LikesController < ApplicationController
-    before_action :find_article 
-    
+    before_action :find_article
+    before_action :find_like, only: [:destroy] 
+    skip_before_action :verify_authenticity_token
+
     def create
-        @like = @article.likes.create(user_id: current_user.id)
+        if already_liked?
+            flash[:notice] = "You can't like more than once"
+        else
+            @like = @article.likes.create(user_id: current_user.id)
+        end
         redirect_to article_path(@article)
+        
+    end
+
+    def destroy
+        if !(already_liked?)
+            flash[:notice] = "cannot unlike"
+        else
+            @like.destroy
+        end
+        redirect_to article_path(@article)
+    end
+
+    def find_like
+        @like = @article.likes.find(params[:id])
     end
 
     private
 
+    def already_liked?
+        Like.where(user_id: current_user.id, article_id:
+        params[:article_id]).exists?
+    end
+
     def find_article
-        @article = @article.find(params[:article_id])
+        @article = Article.find(params[:article_id])
     end
 end
